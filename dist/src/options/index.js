a4b48d43658a51fc7b6ca5fb895bf385e25fa808
Zonaed Hossain <zonaed.ain@gmail.com>
2026-09-01 13:21:58 +0600

    build: include compiled extension dist folder and downloadable dist.zip directly in GitHub repo

---
+++ dist/src/options/index.js
@@ -0,0 +1,2 @@
+import{c as t,j as r}from"../../toaster.js";import{O as e}from"../../App.js";const o=document.getElementById("root");o&&t.createRoot(o).render(r.jsx(e,{}));
+//# sourceMappingURL=index.js.map
